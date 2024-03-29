3d2a96b6b9af23e5061b19db5b452c21e7127542
Pabitra Banerjee <rockstarpabitra2204@gmail.com>
2024-03-30 00:27:16 +0530

    5th video : data types in python

---
+++ datatype.py
@@ -0,0 +1,41 @@
+# Integers
+a = 10
+print(a, type(a))
+
+# Boolean
+b = True
+print(b, type(b))
+
+# Floating Point Number
+c = 2.99E8
+d = 3.14
+
+print(c, type(c))
+print(d, type(d))
+
+# Complex Number
+e = 9+8j
+print(e, type(e))
+
+# String
+f = 'abcdefghijklmnopqrstuvwxyz'
+print(f, type(f))
+print(f[5])
+print(f[-1])
+
+# List
+g = [1, 2, 3, 4, 5, 6, 7, 8]
+print(g, type(g))
+print(g[5])
+print(g[-2])
+
+# Tuples
+h = (1, 2, 3, 4, 5, 6, 7, 8)
+print(h, type(h))
+print(h[5])
+print(h[-3])
+
+# Dictionary
+i = {'a': 1 , 'b': 2 , 'c': 3 , 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12}
+print(i, type(i))
+print(i['e'])
