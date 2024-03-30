b7566c6d148a696ca18a0784dd00cd96c5a442af
Pabitra Banerjee <rockstarpabitra2204@gmail.com>
2024-03-31 02:50:22 +0530

    input function and type casting

---
+++ input-type-casting.py
@@ -0,0 +1,29 @@
+# Int type-casting
+
+a = int(input("Enter Your Age: "))
+b = int(input("Enter Your Father's Age: "))
+print("Your age is: ", a)
+print("The type of the variable a is : ", type(a))
+print("Your father's age is: ", b)
+print("The type of the variable b is : ", type(b))
+print("The sum of your and your father's age is: ", a+b)
+
+# eval type-casting
+
+c = eval(input("Enter Your Age: "))
+d = eval(input("Enter Your Father's Age: "))
+print("Your age is: ", c)
+print("The type of the variable c is : ", type(c))
+print("Your father's age is: ", d)
+print("The type of the variable d is : ", type(d))
+print("The sum of your and your father's age is: ", c+d)
+
+# Eval Function Type-casting
+
+e = eval(input("Enter Your Age: "))
+f = eval(input("Enter Your Father's Age: "))
+print("Your age is: ", e)
+print("The type of the variable e is : ", type(e))
+print("Your father's age is: ", f)
+print("The type of the variable f is : ", type(f))
+print("The sum of your and your father's age is: ", e+f)
