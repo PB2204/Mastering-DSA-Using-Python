21e98f9e639dbe7dc25eda4f7e08347bfd71642a
Pabitra Banerjee <rockstarpabitra2204@gmail.com>
2024-04-03 14:29:09 +0530

    conditional statement

---
+++ if-else.py
@@ -0,0 +1,18 @@
+# if
+# elif - else if
+# else
+# a = int(input("Enter your age: "))
+
+# if a < 18:
+#     print("You can't drive!")
+# else:
+#     print("You can drive!")
+
+b = int(input("Enter a number: "))
+
+if b < 10:
+    print(b, " Is less than 10.")
+elif b > 10:
+    print(b, " Is more than 10.")
+else:
+    print(b, " Is equal to ten.")
